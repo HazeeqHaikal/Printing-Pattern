28ecb20d3692cfc3a56429cdbe9125f8789ad62f
Hazeeq Haikal <43111765+HazeeqHaikal@users.noreply.github.com>
2023-01-07 21:02:03 +0800

    push

---
+++ decreasing triangle/triangle.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    int n = 5;
+
+    for (int i = 1; i <= n; i++) {
+        for (int j = i; j <= n; j++) {
+            cout << "*";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
