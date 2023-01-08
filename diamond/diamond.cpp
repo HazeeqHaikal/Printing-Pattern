f6dd1f87350dfd7613dca4aff79d79b289f82d77
Hazeeq Haikal <43111765+HazeeqHaikal@users.noreply.github.com>
2023-01-08 15:35:19 +0800

    final push

--- diamond/diamond.cpp
+++ diamond/diamond.cpp
@@ -5,6 +5,18 @@ using namespace std;
 
 int main() {
     int n = 5;
+    for (int i = 1; i <= n; i++) {
+        for (int j = i; j <= n; j++) {
+            cout << "  ";
+        }
+        for (int a = 1; a < i; a++) {
+            cout << "* ";
+        }
+        for (int b = 1; b <= i; b++) {
+            cout << "* ";
+        }
+        cout << endl;
+    }
 
     for (int i = 2; i <= n; i++) {
         for (int j = 1; j <= i; j++) {
